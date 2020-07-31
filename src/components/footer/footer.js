c20c457f7f18cd791d7d22ca7140fec9a7492a57
Demetriy Nazimov <ArhDemetriy@GMail.com>
2020-07-31 13:54:41 +0800

    init struct

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  // jsVanila
+  function handler(event) {
+
+  }
+
+  const elements = document.querySelectorAll(`.footer`);
+  elements.forEach((_, i, a) => {
+    // click change
+    // a[i].addEventListener('change', handler, { passive: true, });
+  });
+
+  // jQuery
+  (function ($) {
+
+  })(jQuery);
+
+})();
---
+++ src/components/table/table.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  // jsVanila
+  function handler(event) {
+
+  }
+
+  const elements = document.querySelectorAll(`.table`);
+  elements.forEach((_, i, a) => {
+    // click change
+    // a[i].addEventListener('change', handler, { passive: true, });
+  });
+
+  // jQuery
+  (function ($) {
+
+  })(jQuery);
+
+})();
