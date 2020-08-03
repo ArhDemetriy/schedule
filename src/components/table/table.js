a4e5fdd8ff1887454ca7f401e9f86d94a3a81d77
Demetriy Nazimov <ArhDemetriy@GMail.com>
2020-08-03 22:08:33 +0800

    чтото частично работающее

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -15,13 +15,27 @@
       })
   };
   function setTimeRange(event) {
-    let turn = true;
-    if (event.target.matches('.table__time_begin'))
-      turn = true
-    else if (event.target.matches('.table__time_end'))
-      turn = false
-    else
-      return;
+    if (!(event.target.matches('.table__time_begin') || event.target.matches('.table__time_end'))) return;
+    let targetTime = 0;
+    targetTime = Number(this.querySelector('.table__time_begin').value.replace(':', '.'));
+    targetTime = Math.trunc(targetTime) + (targetTime % 1) * 1.6666;
+    const timeBegin = targetTime;
+    targetTime = Number(this.querySelector('.table__time_end').value.replace(':', '.'));
+    targetTime = Math.trunc(targetTime) + (targetTime % 1) * 1.6666;
+    const timeEnd = targetTime;
+    let beginComplited = false;
+    let endComplited = false;
+
+    let row = this.querySelector('tr');
+    let items = row.querySelectorAll('[data-time]');
+    items.forEach((_, i, a) => {
+      if (beginComplited && (Match.abs(a[i].dataset.time - timeEnd) < 10.4)) endComplited = true;
+      if (!beginComplited && (a[i].dataset.time - timeBegin > -0.49)) beginComplited = true;
+      if (beginComplited && !endComplited)
+        a[i].classList.remove('table__col-hidden')
+      else
+        a[i].classList.add('table__col-hidden');
+    })
 
   };
 
